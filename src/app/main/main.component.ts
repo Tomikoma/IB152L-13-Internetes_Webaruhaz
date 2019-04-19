f3aaf64a166692de53be5022b9b7e477fa5bba2e
Szabó Tamás <szabotomika97@gmail.com>
2019-04-19 20:16:15 +0200

    productlisting added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Product } from './product.model';
+import { Subscription } from 'rxjs';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  products: Product[];
+  private productsSub: Subscription;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getProducts();
+    this.productsSub = this.productService.getProductUpdateListener()
+    .subscribe((products: Product[]) => {
+      this.products = products;
+    });
+  }
+
+  ngOnDestroy() {
+    this.productsSub.unsubscribe();
   }
 
 }
