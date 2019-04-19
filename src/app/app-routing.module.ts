725dc09a14cbb1d64cdbb49f6c1ea0e70c76b937
Szabó Tamás <szabotomika97@gmail.com>
2019-04-19 20:46:41 +0200

    productcomponent added to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ProductComponent } from './main/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'products/:id', component: ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
