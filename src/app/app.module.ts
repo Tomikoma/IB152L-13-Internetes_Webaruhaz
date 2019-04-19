9368b23118cb87ca24c6af7b58ccd9a33f5c916c
Szabó Tamás <szabotomika97@gmail.com>
2019-04-19 18:07:20 +0200

    MainComponent added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListUsersComponent } from './list-users/list-users.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListUsersComponent
+    ListUsersComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
