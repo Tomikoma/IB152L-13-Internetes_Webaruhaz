e38c6ef25c55ce5364a1d74e44ed87d4da7db889
Szabó Tamás <szabotomika97@gmail.com>
2019-04-20 19:47:10 +0200

    productpage added

--- src/app/main/product/product.component.ts
+++ src/app/main/product/product.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 
+import { ProductService } from '../product.service';
+import { TV } from './tv.model';
+import { Smartphone } from './smartphone.model';
+import { Notebook } from './notebook.model';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -10,10 +15,14 @@ import { Subscription } from 'rxjs';
 export class ProductComponent implements OnInit, OnDestroy {
 
   private routeSub: Subscription;
+  private productSub: Subscription;
   productType: string;
   productId: number;
+  tv: TV;
+  smartphone: Smartphone;
+  notebook: Notebook;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
     this.routeSub = this.route.params.subscribe( params => {
       this.productId = params.id;
       this.productType = params.type;
@@ -21,12 +30,25 @@ export class ProductComponent implements OnInit, OnDestroy {
    }
 
   ngOnInit() {
-    console.log(this.productId);
-    console.log(this.productType);
+    this.productService.getProduct(this.productType, this.productId);
+    this.productSub = this.productService.getOneProductUpdateListener()
+      .subscribe(product => {
+        product.RELEASEDATE = new Date(product.RELEASEDATE);
+        if (this.productType === 'tv') {
+          this.tv = product as any as TV;
+        }
+        if(this.productType === 'phone') {
+          this.smartphone = product as any as Smartphone;
+        }
+        if( this.productType === 'notebook') {
+          this.notebook = product as any as Notebook;
+        }
+
+      });
   }
 
   ngOnDestroy() {
-
+    this.productSub.unsubscribe();
   }
 
 }
