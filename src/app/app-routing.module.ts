73cc28148c99c28001c99fd6466b92f3646993cf
Szabó Tamás <szabotomika97@gmail.com>
2019-04-22 10:30:43 +0200

    compare to routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ProductComponent } from './main/product/product.component';
+import { CompareComponent } from './main/product/compare/compare.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
-  {path: 'products/:type/:id', component: ProductComponent}
+  {path: 'products/:type/:id', component: ProductComponent},
+  {path: 'products/:type/:id1/compare/:id2', component: CompareComponent}
 ];
 
 @NgModule({
