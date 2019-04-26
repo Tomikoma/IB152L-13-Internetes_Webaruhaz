0518381251c23b2df2e6cf5d1897c2ce4a89fa12
Szabó Tamás <szabotomika97@gmail.com>
2019-04-26 14:16:58 +0200

    signup implemented

--- backend/services/routes/user.js
+++ backend/services/routes/user.js
@@ -9,13 +9,21 @@ router.post("/signup",async (req,res,next) => {
 
   bcrypt.hash(req.body.password,10)
     .then(async hash =>  {
+      try{
       const result = await database.simpleExecute(
         "INSERT INTO Users(email,password,name,phonenumber,postalcode,city,street,streetnumber) VALUES ('"
         + req.body.email + "','" + hash + "','" + req.body.name + "'," + req.body.phoneNumber + "," + req.body.postalCode
-         + ",'" + req.body.city + "','" + req.body.street + "'," + req.body.streetNumber + ")")
-         .then(response => {
-           console.log(response);
-         });
+         + ",'" + req.body.city + "','" + req.body.street + "'," + req.body.streetNumber + ")");
+         res.status(201).json({
+           message: "User created",
+         })
+      } catch(err) {
+        res.status(500).json({
+          message: 'Something went wrong!'
+        })
+      } finally {
+
+      }
     });
 
 });
