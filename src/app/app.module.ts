a7a47ad04e320c3a4169fa138e4d689a58628019
Szabó Tamás <szabotomika97@gmail.com>
2019-04-28 16:11:03 +0200

    fixes
    -cart component moved
    -imports fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,8 +29,8 @@ import { CompareComponent } from './main/product/compare/compare.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
-import { CartComponent } from './auth/cart/cart.component';
-import { CommentComponent } from './auth/comment/comment.component';
+import { CartComponent } from './cart/cart.component';
+import { CommentComponent } from './main/product/comment/comment.component';
 import { CommentCreateComponent } from './main/product/comment-create/comment-create.component';
 
 @NgModule({
