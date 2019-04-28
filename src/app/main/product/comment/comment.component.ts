f940d92f3cebd3fdfb5d2fe48875507f53d8c3a7
Szabó Tamás <szabotomika97@gmail.com>
2019-04-28 17:24:46 +0200

    Update comment.component.ts
    functions for getting comments implemented

--- src/app/main/product/comment/comment.component.ts
+++ src/app/main/product/comment/comment.component.ts
@@ -1,18 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommentService } from '../comment.service';
+import { MyComment } from '../mycomment.model';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css']
 })
-export class CommentComponent implements OnInit {
+export class CommentComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  comments: MyComment[];
+  productId: number;
+  private commentSub: Subscription;
+
+  constructor(private commentService: CommentService, private route: ActivatedRoute) {
+    this.route.params.subscribe( params => {
+      this.productId = params.id;
+    });
+  }
 
   ngOnInit() {
+    this.commentService.getComments(this.productId);
+    this.commentSub = this.commentService.getCommentsUpdateListener()
+      .subscribe(commentData => {
+        this.comments = commentData.comments;
+        console.log(this.comments);
+      });
   }
+
 
   addComment() {
     return 0;
+  }
+
+  ngOnDestroy() {
+    this.commentSub.unsubscribe();
   }
 }
