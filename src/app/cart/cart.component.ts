a08d3ce7430acafffb83e4be7a24e969c2e90b7f
Szabó Tamás <szabotomika97@gmail.com>
2019-04-29 20:02:09 +0200

    cart componented started to implement

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CartService } from '../main/cart.service';
+import { Subscription } from 'rxjs';
+import { CartItem } from './cartitem.model';
+import { Product } from '../main/product.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private cartUpdateSub:Subscription;
+  cartItems: CartItem[];
+  products: Product[];
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit() {
+    this.cartService.getCartItems();
+    this.cartUpdateSub = this.cartService.getCartUpdateListener().subscribe(cartData => {
+      this.cartItems = cartData.cartItems;
+      this.products = cartData.products;
+    });
   }
 
+  ngOnDestroy() {
+    this.cartUpdateSub.unsubscribe();
+  }
 }
