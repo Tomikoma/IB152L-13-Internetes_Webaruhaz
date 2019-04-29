adf46b97f0e9cff9804f861b6da16eda29582277
Horvath Oliver Zoltan <h.oliver9112@gmail.com>
2019-04-29 16:44:18 +0200

    cart
    -routing added
    -button connected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 import { UserpageComponent } from './userpage/userpage.component';
+import {CartComponent} from './cart/cart.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
@@ -14,7 +15,8 @@ const routes: Routes = [
   {path: 'products/:type/:id1/compare/:id2', component: CompareComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'userinfo', component: UserpageComponent, canActivate: [AuthGuard]}
+  {path: 'userinfo', component: UserpageComponent, canActivate: [AuthGuard]},
+  {path: 'cart', component: CartComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
