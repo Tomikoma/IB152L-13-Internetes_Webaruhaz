a0835dc7a0d94b19310ed8df9d4e387020491c78
Szabó Tamás <szabotomika97@gmail.com>
2019-04-29 19:57:51 +0200

    getCartItems implemented

--- src/app/main/cart.service.ts
+++ src/app/main/cart.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { Product } from './product.model';
+import { CartItem } from '../cart/cartitem.model';
 
 @Injectable({providedIn: 'root'})
 export class CartService {
+
+  products: Product[];
+  cartItems: CartItem[];
+
+  private cartUpdatedListener = new Subject<{products: Product[], cartItems: CartItem[]}>();
 
   constructor(private http: HttpClient) {}
 
@@ -14,10 +22,32 @@ export class CartService {
   }
 
   getCartItems() {
-
+    this.http.get<{products: any[], cartItems: any[]}>('http://localhost:3000/api/products/cart')
+    .subscribe(response => {
+      this.products = response.products.map(product => {
+        return {
+          id: product.ID,
+          productName: product.PRODUCTNAME,
+          productNumber: product.PRODUCTNUMBER,
+          productColor: product.PRODUCTCOLOR,
+          releaseDate: new Date(product.RELEASEDATE),
+          manufacturer: product.MANUFACTURER,
+          price: product.PRICE,
+          quantity: product.QUANTITY,
+          productType: product.PRODUCTTYPE,
+          imgUrl: product.IMGURL
+        };
+      });
+      this.cartItems = response.cartItems;
+      this.cartUpdatedListener.next({products: [...this.products], cartItems: [...this.cartItems]});
+    });
   }
 
   removeFromCart() {
 
+  }
+
+  getCartUpdateListener() {
+    return this.cartUpdatedListener.asObservable();
   }
 }
